c5a92944df1cb0981710c51d7d2e2f74a7cffcd0
unknown <adlindholm@gmail.com>
2023-03-22 11:05:59 +0100

    Cosmetic improvements

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -2,19 +2,20 @@ import os
 import random
 
 def print_menu():
-    os.system('cls')
+    print()
     print('1 --> Sten')
     print('2 --> Sax')
     print('3 --> Påse')
     print('9 --> Ge upp')
+    print()
 
 def compare(weapon1, weapon2):
     result = None
-    if weapon1 == '1' and weapon2 == '2':       #Sten vs. sax
+    if weapon1 == '1' and weapon2 == '2':       # Sten vs. sax
         result = 1
-    elif weapon1 == '1' and weapon2 == '3':     #Sten vs. påse
+    elif weapon1 == '1' and weapon2 == '3':     # Sten vs. påse
         result = 2
-    elif weapon1 == '2' and weapon2 == '1':     #Sax vs. sten
+    elif weapon1 == '2' and weapon2 == '1':     # Sax vs. sten
         result = 2
     elif weapon1 == '2' and weapon2 == '3':     # Sax vs. påse
         result = 1
@@ -23,7 +24,6 @@ def compare(weapon1, weapon2):
     elif weapon1 == '3' and weapon2 == '2':     # Påse vs. sax
         result = 2
 
-    #print(f'Result={result}')
     return result
 
 weapons = {
@@ -34,11 +34,12 @@ weapons = {
 
 
 while True:
+    os.system('cls')
     print_menu()
     player_weapon_of_choice = input("Välj ditt öde: ")
+    os.system('cls')
     if player_weapon_of_choice in('1','2','3'):
         computer_weapon_of_choice = random.randint(1,3)
-        #print(f'Datorns val: {computer_weapon_of_choice}')
         result = compare(player_weapon_of_choice, str(computer_weapon_of_choice))
         if result == 1:
             print('\nDu vann!')
@@ -51,5 +52,5 @@ while True:
         input('\nTryck Enter för en ny runda')
 
     elif player_weapon_of_choice == '9':
-        print('\nAvslutar, tack för visat intresse!\n')
+        print('\nAvslutar...\n')
         break
